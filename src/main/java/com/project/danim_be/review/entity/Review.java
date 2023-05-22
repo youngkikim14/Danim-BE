397375528d1fe1869e16b640bc5f6845763a178c
kimhyunjun0707 <mical151@naver.com>
2023-05-22 16:39:31 +0900

    Feat : Review Entity 추가

--- src/main/java/com/project/danim_be/review/entity/Review.java
+++ src/main/java/com/project/danim_be/review/entity/Review.java
@@ -1,4 +1,22 @@
 package com.project.danim_be.review.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Double point;
+
+	@Column(nullable = false)
+	private String review;
+
 }
