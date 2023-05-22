7d12d7d8d334b3d9b73ae1269898371891cb3944
kimhyunjun0707 <mical151@naver.com>
2023-05-23 06:06:01 +0900

    fix : valid메시지수정

--- src/main/java/com/project/danim_be/member/dto/SignupRequestDto.java
+++ src/main/java/com/project/danim_be/member/dto/SignupRequestDto.java
@@ -12,13 +12,13 @@ import lombok.Setter;
 public class SignupRequestDto {
 
 
-	@Pattern(regexp = "^[a-z0-9_+.-]+@[a-z0-9-]+\\.[a-z0-9]{2,4}$", message = "아이디는 올바른 이메일 형식으로 입력해주세요.")
+	@Pattern(regexp = "^[a-z0-9_+.-]+@[a-z0-9-]+\\.[a-z0-9]{2,4}$", message = "아이디는 올바른 이메일 형식으로 입력해주세요.(ex-danim999@naver.com)")
 	private String userId;
 
-	@Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d!@#$%^&*()_-]{5,12}$", message = "비밀번호정규식")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d!@#$%^&*()_-]{5,12}$", message = "비밀번호는 5~12자이내 영어(소문자),숫자,특수기호(선택) 범위에서 입력해야합니다")
 	private String password;
 
-	@Pattern(regexp ="^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{3,8}$",message="닉네임정규식")
+	@Pattern(regexp ="^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{3,8}$",message="닉네임은 3~8자 이내 한글or영어(대소문자),숫자(선택)범위에서 입력해주세요.특수문자는 포함할수없습니다.")
 	private String nickname;
 
 	private String ageRange;
