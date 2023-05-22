067807a415df0e5e3a7941f1519debb91f2cfb42
kimhyunjun0707 <mical151@naver.com>
2023-05-22 21:52:43 +0900

    Feat : Message 추가

---
+++ src/main/java/com/project/danim_be/common/util/StatusEnum.java
@@ -0,0 +1,13 @@
+package com.project.danim_be.common.util;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum StatusEnum {
+
+	OK(HttpStatus.OK, "OK");
+	private final HttpStatus status;
+	private final String message;
+}
--- src/main/java/com/project/danim_be/member/controller/MemberController.java
+++ src/main/java/com/project/danim_be/member/controller/MemberController.java
@@ -1,4 +1,28 @@
 package com.project.danim_be.member.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.danim_be.common.util.Message;
+import com.project.danim_be.member.dto.SignupRequestDto;
+import com.project.danim_be.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/user")
+@RequiredArgsConstructor
 public class MemberController {
+
+	private final MemberService memberService;
+
+	public ResponseEntity<Message> signup(@RequestBody SignupRequestDto signupRequestDto, BindingResult bindingResult){
+
+		return memberService.signup(signupRequestDto);
+	}
+
+
 }
