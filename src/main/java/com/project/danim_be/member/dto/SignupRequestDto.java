2805c23bebf326d14050ea68480342cf261bc27f
kimhyunjun0707 <mical151@naver.com>
2023-05-23 03:57:45 +0900

    Feat : sinup 추가

--- src/main/java/com/project/danim_be/member/dto/SignupRequestDto.java
+++ src/main/java/com/project/danim_be/member/dto/SignupRequestDto.java
@@ -1,4 +1,29 @@
 package com.project.danim_be.member.dto;
 
+import org.thymeleaf.spring6.processor.SpringInputGeneralFieldTagProcessor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupRequestDto {
+
+
+	@Pattern(regexp = "^[a-z0-9_+.-]+@[a-z0-9-]+\\.[a-z0-9]{2,4}$", message = "아이디는 올바른 이메일 형식으로 입력해주세요.")
+	private String userId;
+
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d!@#$%^&*()_-]{5,12}$", message = "비밀번호정규식")
+	private String password;
+
+	@Pattern(regexp ="^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{3,8}$",message="닉네임정규식")
+	private String nickname;
+
+	private String ageRange;
+
+
+
+
 }
--- src/main/java/com/project/danim_be/member/service/MemberService.java
+++ src/main/java/com/project/danim_be/member/service/MemberService.java
@@ -1,12 +1,50 @@
 package com.project.danim_be.member.service;
 
+import java.util.Optional;
+
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.project.danim_be.common.util.Message;
+import com.project.danim_be.common.util.StatusEnum;
 import com.project.danim_be.member.dto.SignupRequestDto;
+import com.project.danim_be.member.entity.Member;
+import com.project.danim_be.member.repository.MemberRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+
+
 	public ResponseEntity<Message> signup(SignupRequestDto signupRequestDto) {
+
+		String userId 	= signupRequestDto.getUserId();
+		String password = passwordEncoder.encode(signupRequestDto.getPassword());
+		String nickname	= signupRequestDto.getNickname();
+		String ageRange = signupRequestDto.getAgeRange();
+
+		Optional<Member> findMember = memberRepository.findByUserId(userId);
+		if(findMember.isPresent()){
+			throw new RuntimeException("이미 가입된 아이디 입니다");
+		}
+		Optional<Member> foundMemberNickname = memberRepository.findByNickname(nickname);
+		if (foundMemberNickname.isPresent()) {
+			throw new RuntimeException("중복된 닉네임입니다.");
+		}
+		Member member =  new Member(userId,password,nickname,ageRange);
+		memberRepository.save(member);
+
+		Message message = Message.setSuccess(StatusEnum.OK,"회원 가입 성공");
+		return new ResponseEntity<>(message, HttpStatus.OK);
 
 
 	}
