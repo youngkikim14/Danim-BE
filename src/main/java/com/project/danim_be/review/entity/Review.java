fed9c4e996abddf83d55c75f84c490cf02245a83
kimhyunjun0707 <mical151@naver.com>
2023-05-22 16:57:06 +0900

    Feat :Entitiy Mapping

--- src/main/java/com/project/danim_be/review/entity/Review.java
+++ src/main/java/com/project/danim_be/review/entity/Review.java
@@ -1,10 +1,15 @@
 package com.project.danim_be.review.entity;
 
+import com.project.danim_be.member.entity.Member;
+import com.project.danim_be.post.entity.Post;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Review {
@@ -14,9 +19,15 @@ public class Review {
 	private Long id;
 
 	@Column(nullable = false)
-	private Double point;
+	private Double point;		//점수
 
 	@Column(nullable = false)
-	private String review;
+	private String review;		//후기
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Post post;
 
 }
