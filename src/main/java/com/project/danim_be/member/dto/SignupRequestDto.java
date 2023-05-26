09d12b51ae69b8b3def738c7672603e81114874a
youngkikim <rkfcl1169@gmail.com>
2023-05-27 05:15:21 +0900

    Fix : Signup fix

--- src/main/java/com/project/danim_be/member/dto/SignupRequestDto.java
+++ src/main/java/com/project/danim_be/member/dto/SignupRequestDto.java
@@ -3,11 +3,13 @@ package com.project.danim_be.member.dto;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequestDto {
 
 
