cfb19b9d1a06d7640f94bd81c06a9f6bbbb2d189
kimhyunjun0707 <mical151@naver.com>
2023-05-26 17:59:00 +0900

    Fix : Jasypt Test18

--- src/test/java/com/project/danim_be/DanimBeApplicationTests.java
+++ src/test/java/com/project/danim_be/DanimBeApplicationTests.java
@@ -2,10 +2,8 @@ package com.project.danim_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(properties = {"jasypt.password=${JASYPT_PASSWORD:testpassword}"})
-
 class DanimBeApplicationTests {
 
     @Test
