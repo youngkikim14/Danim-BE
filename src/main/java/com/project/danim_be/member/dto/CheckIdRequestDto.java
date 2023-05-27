4b8be94d738753e505096a205b0fba60a0ed8d2e
limseulki <limslki333@hanmail.net>
2023-05-27 18:35:13 +0900

    Feat : 일반 회원가입 아이디/닉네임 중복 검사 기능 추가

---
+++ src/main/java/com/project/danim_be/member/dto/CheckIdRequestDto.java
@@ -0,0 +1,10 @@
+package com.project.danim_be.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CheckIdRequestDto {
+    private String userId;
+}
