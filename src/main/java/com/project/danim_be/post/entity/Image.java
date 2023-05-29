06a679811d72c005f26b4dca0c15dc3d96a2428e
kimhyunjun0707 <mical151@naver.com>
2023-05-29 19:11:27 +0900

    Feat : Image Entity

--- src/main/java/com/project/danim_be/post/entity/Image.java
+++ src/main/java/com/project/danim_be/post/entity/Image.java
@@ -1,37 +1,20 @@
 package com.project.danim_be.post.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity
-public class Content {
+public class Image {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String type;
+	private String imageUrl;
 
-	private String level;
-
-	private String text;
-
-	@OneToMany
-	private List<Image> imageLists;
-
-	private String alt; //..?
-
-
-
-
-
-
-
+	private String imageName;
 
 
 }
