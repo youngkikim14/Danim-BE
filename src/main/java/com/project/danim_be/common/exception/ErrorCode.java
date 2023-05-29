43333f6dfd321951c0b3c6957317d764e7a0fbe2
kimhyunjun0707 <mical151@naver.com>
2023-05-30 01:37:35 +0900

    Feat : S3 Uploader

--- src/main/java/com/project/danim_be/common/exception/ErrorCode.java
+++ src/main/java/com/project/danim_be/common/exception/ErrorCode.java
@@ -15,6 +15,8 @@ public enum ErrorCode {
 	INVALID_PASSWORD(HttpStatus.BAD_REQUEST,"잘못된 비밀번호 입니다.","사용자 등록 오류입니다."),
 	DELETED_USER(HttpStatus.BAD_REQUEST, "탈퇴한 사용자입니다.", "사용자 등록 오류입니다."),
 	FAIL_SIGNOUT(HttpStatus.BAD_REQUEST, "탈퇴에 실패했습니다.", "사용자 등록 오류입니다."),
+	//파일등록
+	FILE_CONVERT_FAIL(HttpStatus.BAD_REQUEST,"파일 변환에 실패했습니다","파일 등록 오류입니다"),
 	//404
 	ID_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 아이디 입니다.","사용자 등록 오류입니다."),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "찾을수 없는 회원입니다.","사용자 등록 오류입니다.");
--- src/main/java/com/project/danim_be/post/service/PostService.java
+++ src/main/java/com/project/danim_be/post/service/PostService.java
@@ -1,4 +1,26 @@
 package com.project.danim_be.post.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.project.danim_be.member.entity.Member;
+import com.project.danim_be.member.repository.MemberRepository;
+import com.project.danim_be.post.dto.PostRequestDto;
+import com.project.danim_be.post.repository.PostRepository;
+import com.project.danim_be.security.auth.UserDetailsImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class PostService {
+
+	private final PostRepository postRepository;
+	private final MemberRepository memberRepository;
+	//게시글작성
+	@Transactional
+	public void createPost(Member member, PostRequestDto requestDto) {
+
+
+	}
 }
