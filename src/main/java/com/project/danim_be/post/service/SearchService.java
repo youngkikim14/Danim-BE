7b568b1dfeba4b4b601c90df11966fe53d30059a
youngkikim <rkfcl1169@gmail.com>
2023-05-31 02:57:30 +0900

    Test : Add AllPost Test

--- src/main/java/com/project/danim_be/post/service/SearchService.java
+++ src/main/java/com/project/danim_be/post/service/SearchService.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
