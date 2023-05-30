63dde81c2fb622b02acf016eeb146a1e1f32ec5f
limseulki <limslki333@hanmail.net>
2023-05-30 21:05:01 +0900

    Feat : Swagger Controller 설명 추가

--- src/main/java/com/project/danim_be/review/controller/ReviewController.java
+++ src/main/java/com/project/danim_be/review/controller/ReviewController.java
@@ -4,6 +4,8 @@ import com.project.danim_be.common.util.Message;
 import com.project.danim_be.review.dto.ReviewRequestDto;
 import com.project.danim_be.review.service.ReviewService;
 import com.project.danim_be.security.auth.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "ReviewController", description = "여행 리뷰 - 댓글 API")
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @Operation(summary = "리뷰 작성 API", description = "리뷰 작성")
     @PostMapping("api/post/{postId}/review")
     public ResponseEntity<Message> createReview(@PathVariable Long postId, @RequestBody ReviewRequestDto reviewRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return reviewService.createReview(postId, reviewRequestDto, userDetails.getMember());
