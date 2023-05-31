3859e4313f9bda986ed0de7ad6d8333eb8bde02b
limseulki <limslki333@hanmail.net>
2023-06-01 01:37:16 +0900

    Fix : Timestamped 작동하도록 어노테이션 추가

--- src/main/java/com/project/danim_be/DanimBeApplication.java
+++ src/main/java/com/project/danim_be/DanimBeApplication.java
@@ -1,11 +1,13 @@
 package com.project.danim_be;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableEncryptableProperties
 public class DanimBeApplication {
 
     public static void main(String[] args) {
