e75ad6da18f5c1194a4172f64f091dbbd708f433
limseulki <limslki333@hanmail.net>
2023-06-01 02:00:48 +0900

    Fix : Timestamped 작동하도록 재수정

--- src/main/java/com/project/danim_be/DanimBeApplication.java
+++ src/main/java/com/project/danim_be/DanimBeApplication.java
@@ -3,11 +3,12 @@ package com.project.danim_be;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
-@EnableEncryptableProperties
 public class DanimBeApplication {
 
     public static void main(String[] args) {
