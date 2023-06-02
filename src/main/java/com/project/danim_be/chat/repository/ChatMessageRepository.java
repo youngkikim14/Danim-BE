73942879cc07e01b59f76490a80e86899df62eae
kimhyunjun0707 <mical151@naver.com>
2023-06-03 03:04:59 +0900

    Changes by SEULKI _-_

--- src/main/java/com/project/danim_be/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/project/danim_be/chat/repository/ChatMessageRepository.java
@@ -1,4 +1,8 @@
 package com.project.danim_be.chat.repository;
 
-public interface ChatMessageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.danim_be.chat.entity.ChatMessage;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 }
--- src/main/java/com/project/danim_be/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/project/danim_be/chat/repository/ChatRoomRepository.java
@@ -1,4 +1,9 @@
 package com.project.danim_be.chat.repository;
 
-public interface ChatRoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import com.project.danim_be.chat.entity.ChatRoom;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, QuerydslPredicateExecutor<ChatRoom> {
 }
