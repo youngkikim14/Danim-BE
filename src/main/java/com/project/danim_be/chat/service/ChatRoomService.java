7b76d73d0fb6568835fd26c48daa579a19e14150
limseulki <limslki333@hanmail.net>
2023-06-04 04:24:11 +0900

    Feat : 신청시 참여인원 증가 기능 추가

--- src/main/java/com/project/danim_be/chat/service/ChatRoomService.java
+++ src/main/java/com/project/danim_be/chat/service/ChatRoomService.java
@@ -1,30 +1,35 @@
 package com.project.danim_be.chat.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
 import com.project.danim_be.chat.dto.ChatRoomResponseDto;
 import com.project.danim_be.chat.entity.ChatRoom;
 import com.project.danim_be.chat.entity.QMemberChatRoom;
+import com.project.danim_be.chat.repository.ChatRoomRepository;
+import com.project.danim_be.chat.repository.MemberChatRoomRepository;
+import com.project.danim_be.common.exception.CustomException;
+import com.project.danim_be.common.exception.ErrorCode;
 import com.project.danim_be.common.util.Message;
 import com.project.danim_be.common.util.StatusEnum;
 import com.project.danim_be.member.entity.Member;
 import com.project.danim_be.post.entity.Post;
 import com.project.danim_be.post.repository.PostRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
 
+	private final MemberChatRoomRepository memberChatRoomRepository;
+	private final ChatRoomRepository chatRoomRepository;
 	private final PostRepository postRepository;
 	private final JPAQueryFactory queryFactory;
 
+	//내가 쓴글의 채팅방 목록조회
 	public ResponseEntity<Message> myChatRoom(Long id) {
 		List<Post> postList = postRepository.findByMember_Id(id);
 		List<ChatRoomResponseDto> chatRoomResponseDtoList = new ArrayList<>();
@@ -55,6 +60,16 @@ public class ChatRoomService {
 
 	//채팅방 참여(웹소켓연결/방입장) == 매칭 신청 버튼
 	public ResponseEntity<Message> joinChatRoom(Long id, Member member) {
-		return null;
+		ChatRoom chatRoom = chatRoomRepository.findById(id).orElseThrow(
+				() -> new CustomException(ErrorCode.ROOM_NOT_FOUND)
+		);
+
+		// 작성자가 아니고?? 방에 처음 들어온다면 참여인원 +1
+		if(!memberChatRoomRepository.existsByMember_IdAndChatRoom_RoomId(member.getId(), chatRoom.getRoomId())){
+			Post post = postRepository.findByChatRoom_Id(id);
+			post.incNumberOfParticipants();
+		}
+
+		return ResponseEntity.ok(Message.setSuccess(StatusEnum.OK,"채팅방 입장"));
 	}
 }
