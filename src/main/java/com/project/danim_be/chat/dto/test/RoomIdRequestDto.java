636f93896170aa13cf640ef5b9ba353a9cb0630f
kimhyunjun0707 <mical151@naver.com>
2023-06-07 21:08:12 +0900

    Fix : StompTest.html 수정

---
+++ src/main/java/com/project/danim_be/chat/dto/test/RoomIdRequestDto.java
@@ -0,0 +1,17 @@
+package com.project.danim_be.chat.dto.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomIdRequestDto {
+	String roomId;
+
+}
