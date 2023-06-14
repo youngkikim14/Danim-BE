e48b6f0874a99e626e1d64bbeda6d30f41176420
kimhyunjun0707 <114275003+kimhyunjun0707@users.noreply.github.com>
2023-06-15 06:43:45 +0900

    Update PostResponseDto.java

--- src/main/java/com/project/danim_be/post/dto/ResponseDto/PostResponseDto.java
+++ src/main/java/com/project/danim_be/post/dto/ResponseDto/PostResponseDto.java
@@ -14,29 +14,30 @@ import java.util.List;
 public class PostResponseDto {
 
 
-	private Long 		postId;					//게시글번호
-	private String		nickName;				//닉네임
-	private String 		postTitle;				//게시글제목
+	private Long 		postId;				//게시글번호
+	private String		nickName;			//닉네임
+	private String 		postTitle;			//게시글제목
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private Date recruitmentStartDate;			//모집 시작날짜
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private Date recruitmentEndDate;			//모집 마감날짜
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-	private Date tripStartDate;					//여행 시작날짜
+	private Date tripStartDate;				//여행 시작날짜
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-	private Date tripEndDate;					//여행 마감날짜
+	private Date tripEndDate;				//여행 마감날짜
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime createdAt;			//게시글 작성시간
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime modifiedAt;			//게시글 수정시간
-	private Long chatRoomId;					//채팅방아이디
-	private String map;							//지도 api 정보
-	private int groupSize;						//인원수
-	private String keyword;						//키워드
-	private String location;					//지역
+	private String content;					//게시글
+	private Long chatRoomId;				//채팅방아이디
+	private String map;					//지도 api 정보
+	private int groupSize;					//인원수
+	private String keyword;					//키워드
+	private String location;				//지역
 	private List<String> ageRange;				//연령대
 	private List<String> gender;				//성별
-	private String content;						//게시글
+	private Integer numberOfParticipants ;			//현재모집인원수
 
 
 
@@ -58,5 +59,6 @@ public class PostResponseDto {
 		this.chatRoomId = post.getChatRoom().getId();
 		this.createdAt = post.getCreatedAt();
 		this.modifiedAt = post.getModifiedAt();
+		this.numberOfParticipants= post.getNumberOfParticipants();
 	}
 }
