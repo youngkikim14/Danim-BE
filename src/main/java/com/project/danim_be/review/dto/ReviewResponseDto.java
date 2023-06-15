5dfb34ff9c2c2d412e73968cbd02e96d7b874ace
limseulki <limslki333@hanmail.net>
2023-06-16 02:15:08 +0900

    fix : review 조회시 프로필 사진, 작성일 포함되도록 수정

--- src/main/java/com/project/danim_be/review/dto/ReviewResponseDto.java
+++ src/main/java/com/project/danim_be/review/dto/ReviewResponseDto.java
@@ -1,22 +1,30 @@
 package com.project.danim_be.review.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.danim_be.review.entity.Review;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ReviewResponseDto {
 
     private Long id;
+    private String userImageUrl;
     private String nickname;
     private String comment;
     private Double score;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 
     public ReviewResponseDto(Review review) {
         this.id = review.getId();
+        this.userImageUrl = review.getMember().getImageUrl();
         this.nickname = review.getMember().getNickname();
         this.comment = review.getReview();
         this.score = review.getPoint();
+        this.createdAt = review.getCreatedAt();
     }
 }
