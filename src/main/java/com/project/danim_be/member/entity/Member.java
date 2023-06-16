ae8c62561cc19884b23d7c73d22aa9a824466504
youngkikim14 <127404498+youngkikim14@users.noreply.github.com>
2023-06-16 19:03:36 +0900

    Update Member.java

--- src/main/java/com/project/danim_be/member/entity/Member.java
+++ src/main/java/com/project/danim_be/member/entity/Member.java
@@ -66,11 +66,6 @@ public class Member extends Timestamped {
 		this.nickname = mypageRequestDto.getNickname();
 	}
 
-	public void editNoImageMember (MypageRequestDto mypageRequestDto) {
-		this.content = mypageRequestDto.getContent();
-		this.nickname = mypageRequestDto.getNickname();
-	}
-
 
 	public String getUserId() {
 		return userId;
