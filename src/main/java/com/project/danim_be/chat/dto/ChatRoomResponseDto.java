2acf42ba012f0b4840b35f9b54fa1eb8da195084
kimhyunjun0707 <114275003+kimhyunjun0707@users.noreply.github.com>
2023-06-19 05:15:34 +0900

    Update ChatRoomResponseDto.java

--- src/main/java/com/project/danim_be/chat/dto/ChatRoomResponseDto.java
+++ src/main/java/com/project/danim_be/chat/dto/ChatRoomResponseDto.java
@@ -1,24 +1,38 @@
 package com.project.danim_be.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.danim_be.chat.entity.ChatMessage;
 import com.project.danim_be.chat.entity.ChatRoom;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 public class ChatRoomResponseDto {
 
-	private Long id;
-
+	private Long roomId;
 	private String postTitle;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date createAtTime;
+	private String roomName;
+	private ChatMessage lastMessage;
 
-	private LocalDateTime createdTime;
 
 	public ChatRoomResponseDto(ChatRoom chatRoom) {
-		this.id = chatRoom.getId();
+		this.roomId = chatRoom.getId();
 		this.postTitle = chatRoom.getPost().getPostTitle();
-		this.createdTime = chatRoom.getPost().getCreatedAt();
+		this.createAtTime = chatRoom.getPost().getCreatedAt();
+		this.roomName = chatRoom.getRoomName();
+		List<ChatMessage> chatMessages = chatRoom.getChatMessages();
+		if (!chatMessages.isEmpty()) {
+			chatMessages.sort(Comparator.comparing(ChatMessage::getCreatedAt).reversed());
+			this.lastMessage = chatMessages.get(0);
+		} else {
+			this.lastMessage = null;
+		}
 	}
 }
