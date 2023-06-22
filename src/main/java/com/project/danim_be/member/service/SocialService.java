e844aece40f7de0e7192bd0c408ce0731ce64952
limseulki <limslki333@hanmail.net>
2023-06-22 19:29:11 +0900

    Chore : 카카오로그인 프로필이미지 가져오는 기능 롤백
    프론트 작업 들어가기 전이라 배포된 서버에서 카카오 로그인 가능하도록 되돌려 놓습니다.

--- src/main/java/com/project/danim_be/member/service/SocialService.java
+++ src/main/java/com/project/danim_be/member/service/SocialService.java
@@ -162,7 +162,7 @@ public class SocialService {
         JsonNode userInfoData = objectMapper.readTree(responseBody);
         System.out.println("userInfoData : " + userInfoData);
         String email = null;
-        String userImage = null;
+//        String userImage = null;
 
         switch (provider) {
             case "NAVER" -> {
@@ -172,8 +172,8 @@ public class SocialService {
             case "KAKAO" -> {
                 email = userInfoData.get("kakao_account").get("email").asText();
                 System.out.println("email : "+email);
-                userImage = userInfoData.get("kakao_account").get("profile").get("profile_image_url").asText();
-                System.out.println("image : "+userImage);
+//                userImage = userInfoData.get("kakao_account").get("profile").get("profile_image_url").asText();
+//                System.out.println("image : "+userImage);
                 break;
             }
             case "GOOGLE" -> {
@@ -181,30 +181,8 @@ public class SocialService {
                 break;
             }
         }
-        return new MemberRequestDto(email, userImage);
+        return new MemberRequestDto(email);
     }
-
-
-//    userInfoData :
-//    {
-//        "id":2812358300,
-//        "connected_at":"2023-06-22T09:30:40Z",
-//        "properties":{
-//            "profile_image":"http://k.kakaocdn.net/dn/CNtB5/btsgEbkETvP/kt9TvnsVi0GTVD643Sjmck/img_640x640.jpg",
-//            "thumbnail_image":"http://k.kakaocdn.net/dn/CNtB5/btsgEbkETvP/kt9TvnsVi0GTVD643Sjmck/img_110x110.jpg"
-//        },
-//        "kakao_account":{
-//            "profile_image_needs_agreement":false,
-//            "profile":{
-//                "thumbnail_image_url":"http://k.kakaocdn.net/dn/CNtB5/btsgEbkETvP/kt9TvnsVi0GTVD643Sjmck/img_110x110.jpg",
-//                "profile_image_url":"http://k.kakaocdn.net/dn/CNtB5/btsgEbkETvP/kt9TvnsVi0GTVD643Sjmck/img_640x640.jpg",
-//                "is_default_image":false},"has_email":true,
-//                "email_needs_agreement":false,"is_email_valid":true,
-//                "is_email_verified":true,
-//                "email":"doh_0112@naver.com"
-//        }
-//    }
-
 
     private Member saveMember(MemberRequestDto memberRequestDto, String provider) {
 
@@ -220,15 +198,16 @@ public class SocialService {
                     .password(password)
                     .isDeleted(false)
                     .score(20.0)
-                    .imageUrl(memberRequestDto.getUserImage())
+                    .imageUrl("https://danimdata.s3.ap-northeast-2.amazonaws.com/avatar.png")
+//                    .imageUrl(memberRequestDto.getUserImage())
                     .build();
 
             memberRepository.save(member);
             return member;
-        } else {
-            // 재로그인시 프로필 정보가 바뀌었다면? 이메일은 바뀔리 없지만 사진은 바뀔수도 있지? 사진만 업데이트? ㅇㅋ
-            socialMember.setImageUrl(memberRequestDto.getUserImage());
-            memberRepository.save(socialMember);
+//        } else {
+//            // 재로그인시 프로필 정보가 바뀌었다면? 이메일은 바뀔리 없지만 사진은 바뀔수도 있지? 사진만 업데이트? ㅇㅋ
+//            socialMember.setImageUrl(memberRequestDto.getUserImage());
+//            memberRepository.save(socialMember);
         }
         return socialMember;
     }
