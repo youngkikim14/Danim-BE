8afc837d063c4b000dbce13f131995da7ea4cc94
Seulki <97998858+limseulki@users.noreply.github.com>
2023-06-23 17:12:12 +0900

    Update SocialService.java

--- src/main/java/com/project/danim_be/member/service/SocialService.java
+++ src/main/java/com/project/danim_be/member/service/SocialService.java
@@ -172,7 +172,12 @@ public class SocialService {
             case "KAKAO" -> {
                 email = userInfoData.get("kakao_account").get("email").asText();
                 if(userInfoData.get("kakao_account").get("profile_image_needs_agreement").asText().equals("false")) {
-                    userImage = userInfoData.get("kakao_account").get("profile").get("profile_image_url").asText();
+                    if(!userInfoData.get("kakao_account").get("profile").get("profile_image_url").isNull()){
+                        userImage = userInfoData.get("kakao_account").get("profile").get("profile_image_url").asText();
+                    }
+                    else {
+                        userImage = "https://danimdata.s3.ap-northeast-2.amazonaws.com/avatar.png";
+                    }
                 } else {
                     userImage = "https://danimdata.s3.ap-northeast-2.amazonaws.com/avatar.png";
                 }
