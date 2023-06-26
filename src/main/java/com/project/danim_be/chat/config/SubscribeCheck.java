5737f45becdf205b29345b93683f76008f8e0b35
kimhyunjun0707 <mical151@naver.com>
2023-06-27 04:49:22 +0900

    fix : Alarm수정

--- src/main/java/com/project/danim_be/chat/config/SubscribeCheck.java
+++ src/main/java/com/project/danim_be/chat/config/SubscribeCheck.java
@@ -1,22 +1,24 @@
 package com.project.danim_be.chat.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.messaging.Message;
-	import org.springframework.messaging.MessageChannel;
-	import org.springframework.messaging.simp.stomp.StompCommand;
-	import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-	import org.springframework.messaging.support.ChannelInterceptor;
-	import org.springframework.messaging.support.MessageHeaderAccessor;
-	import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.stereotype.Component;
 
-import com.project.danim_be.chat.service.ChatMessageService;
 
 @Component
 public class SubscribeCheck implements ChannelInterceptor {
 
-	@Autowired
-	private ChatMessageService chatMessageService;
+	private ApplicationEventPublisher eventPublisher;
+
+	public SubscribeCheck(ApplicationEventPublisher eventPublisher) {
+		this.eventPublisher = eventPublisher;
+	}
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -26,11 +28,22 @@ public class SubscribeCheck implements ChannelInterceptor {
 			String destination = accessor.getDestination();
 			// destination에서 userId를 파싱하고,
 			Long userId = parseUserIdFromDestination(destination);
-			chatMessageService.alarmList(userId);
+			// 이벤트 발행
+			eventPublisher.publishEvent(new SubscriptionEvent(userId));
 		}
 		return message;
 	}
+	public class SubscriptionEvent {
+		private Long userId;
 
+		public SubscriptionEvent(Long userId) {
+			this.userId = userId;
+		}
+
+		public Long getUserId() {
+			return userId;
+		}
+	}
 	private Long parseUserIdFromDestination(String destination) {
 		String[] parts = destination.split("/");
 		return Long.parseLong(parts[parts.length - 1]);
