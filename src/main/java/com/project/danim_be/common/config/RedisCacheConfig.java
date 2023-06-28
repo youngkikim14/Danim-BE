46ee456c9096b3d8a8000a4403bfc8294bb5b90f
youngkikim <rkfcl1169@gmail.com>
2023-06-29 03:20:32 +0900

    Feat : Redis cluster add

--- src/main/java/com/project/danim_be/common/config/RedisCacheConfig.java
+++ src/main/java/com/project/danim_be/common/config/RedisCacheConfig.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
