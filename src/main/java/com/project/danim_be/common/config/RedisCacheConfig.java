2be02236a85bab0da4db7ac18ba8e29c9dce5d99
youngkikim14 <127404498+youngkikim14@users.noreply.github.com>
2023-07-01 01:56:27 +0900

    Update RedisCacheConfig.java

--- src/main/java/com/project/danim_be/common/config/RedisCacheConfig.java
+++ src/main/java/com/project/danim_be/common/config/RedisCacheConfig.java
@@ -13,14 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
