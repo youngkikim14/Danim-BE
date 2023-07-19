4b9ffaf29476894e6326bc17ad0c482eef3da707
youngkikim14 <127404498+youngkikim14@users.noreply.github.com>
2023-07-20 00:00:22 +0900

    Update MemberService.java

--- src/main/java/com/project/danim_be/member/service/MemberService.java
+++ src/main/java/com/project/danim_be/member/service/MemberService.java
@@ -199,8 +199,8 @@ public class MemberService {
 
 		Cookie refreshTokenCookie = new Cookie("REFRESH_KEY", tokenDto.getRefreshToken().substring(7));
 		refreshTokenCookie.setHttpOnly(true);
-//		refreshTokenCookie.setSecure(true);
-		refreshTokenCookie.setDomain("da-nim.com");
+		refreshTokenCookie.setSecure(true);
+		refreshTokenCookie.setDomain(".da-nim.com");
 		refreshTokenCookie.setPath("/");
 		response.addCookie(refreshTokenCookie);
 
